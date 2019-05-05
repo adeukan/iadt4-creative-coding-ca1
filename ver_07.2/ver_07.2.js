3005defdfa02b744d3005c992eafc0bab509b4f2
Vitalijs Komarovs <adeukan@gmail.com>
2019-05-05 14:28:26 +0100

    ver_07.2

--- ver_07.2/ver_07.2.js
+++ ver_07.2/ver_07.2.js
@@ -1,30 +1,21 @@
 'use strict'
 let topColors = [];
 let lowColors = [];
-let numOfCols = 7;
-let numOfRows = 10;
-let interPoint;
-let d;
-
-// they are global again :) 
 let outerCircles = [];
 let innerCircles = [];
-
-// pseudo random set number
+let numOfCols = 7;
+let numOfRows = 10;
 let actRandomSeed = 0;
-// creates additional hidden columns, allows to control the offset
-let offsetSize = 0;
-
-
+// selected offset value
+let offsetSize = 3;
+let interPoint;
+let d;
 
 function setup() {
     let scale = 75;
     createCanvas(7 * scale, 10 * scale);
     colorMode(HSB, 360, 100, 100, 100);
-    // no stroke anymore
     noStroke();
-    // moved to draw()
-    // shakeColors();
 }
 
 function draw() {
@@ -32,54 +23,31 @@ function draw() {
     clear();
     noLoop();
     let tileSize = width / numOfCols;
-
-    // colour palette
-    let palette = [];
-
-    // moved here to enable pseudo-random sets
     shakeColors();
-
-    // they are global again :) 
-    // var outerCircles = [];
-    // var innerCircles = [];
 
     for (let gridX = 0; gridX <= numOfCols; gridX++) {
 
         outerCircles[gridX] = [];
         innerCircles[gridX] = [];
 
-        // now the loop can draw additional hidden rows (offsetSize)
         for (let gridY = 0; gridY <= numOfRows + offsetSize; gridY++) {
 
             let centerX = gridX * tileSize;
             let centerY = gridY * tileSize;
             d = 1.33 * tileSize;
 
-            // [numOfRows - gridY] try later !
-            // use vertical position of outer circle to get its interpolation colour
             let outerPos = map(gridY, 0, numOfRows, 0, 1);
             let outerColor = lerpColor(topColors[gridX], lowColors[gridX], outerPos);
-
-            // use vertical position of inner circle to get its interpolation colour
             let innerPos = map(gridY, 0, numOfRows, 0, 1);
             let innerColor = lerpColor(topColors[gridX], lowColors[gridX], innerPos);
-
-            let outerCircle = new Circle(centerX, centerY, d, outerColor);
-            // let innerCircle = new Circle(centerX, centerY, d / 2, innerColor);
+            // interpolation is temporary removed (topColors[gridX] instead of outerColor)
+            let outerCircle = new Circle(centerX, centerY, d, topColors[gridX]);
+            let innerCircle = new Circle(centerX, centerY, d / 2, innerColor);
             outerCircles[gridX].push(outerCircle);
-            // innerCircles[gridX].push(innerCircle);
+            innerCircles[gridX].push(innerCircle);
 
-            // strokeWeight(1);
-            // stroke(0);
-            // blendMode(ADD);
-
-            // this loop is not used for drawing anymore
-            // fill(outerCircle.c, 100);
-            // ellipse(outerCircle.x, outerCircle.y, outerCircle.d, outerCircle.d);
-            // fill(innerCircle.c, 0);
-            // ellipse(innerCircle.x, innerCircle.y, innerCircle.d, innerCircle.d);
-
-            palette.push(outerCircle.c);
+            // the palette will be created in another way
+            // palette.push(outerCircle.c);
 
             if (gridX === 0 && gridY === 1) {
                 let points = getIntersectPoints(outerCircles[gridX][gridY], outerCircles[gridX][gridY - 1]);
@@ -88,45 +56,43 @@ function draw() {
         }
     }
 
-    // index of currently used palette colour
-    let colorCounter = 0;
+    // fill the palette by moving through the ROWS of circles (including outside the canvas)
+    let palette = [];
+    for (let i = 0; i <= numOfCols; i++) {
+        for (let j = 0; j < outerCircles.length; j++) {
+            palette.push(outerCircles[j][i].c);
+        }
+    }
 
-    // redraw canvas with the offset
+
+    // draw
+    let colorCounter = 0;
     for (let gridX = 0; gridX <= numOfCols; gridX++) {
 
-        // redraw all circles excluding the circles outside the canvas
         outerCircles[gridX].forEach(function(circle, index) {
 
-            // draw only visible circles in every column,
-            // but still use all the colors from the palette consistently
-            let BreakException = {};
-            try {
-                // break drawing the column if circle is outside the canvas
-                if (index > numOfRows) throw BreakException;
+            // in this version we don't need to control hidden rows of circles
+            // let BreakException = {};
+            // try {
+            //     if (index > numOfRows) throw BreakException;
 
-            // color index should not exceed the palette range
             let colorIndex = colorCounter % palette.length;
-
-            // draw circles
             fill(palette[colorIndex]);
             ellipse(circle.x, circle.y, circle.d, circle.d);
-
             colorCounter++;
 
-            } catch (e) {
-                if (e !== BreakException) throw e;
-            }
+            // } catch (e) {
+            //     if (e !== BreakException) throw e;
+            // }
         });
     }
 
-    // draw arcs
     drawArcs();
 }
 
-// change the set of pseudo-random numbers used to generate top\lower colour pairs
 function mouseReleased() {
 
-    // actRandomSeed = random(100000);
+    actRandomSeed = random(100000);
 
     // select the optimal offset
     offsetSize++;
@@ -141,30 +107,18 @@ function shakeColors() {
 
     let topHue;
     let lowHue;
-
-    // a larger multiplier gives more colours and offset combinations
     let mult = 3;
 
-    // in order for the offset to work properly,
-    // the number of top\lower colour pairs must exceed the number of columns
     for (let i = 0; i <= numOfCols * mult; i++) {
-
-        // 1st circle in the top row has random colour 
         if (i === 0) {
             topHue = random(360);
             topColors[i] = color(topHue, 100, 100);
 
-            // next 3 circles in the top row complete the square colour scheme based on the 1st random colour
-            // each next lower colour equal to previous top
         } else if (i < 4) {
-
-            // quare colour scheme
             topHue += 90;
             topHue = topHue > 360 ? topHue - 360 : topHue;
-
             topColors[i] = color(topHue, 100, 100);
             lowColors[i] = topColors[i - 1];
-            // all the rest colours are based on previous
         } else {
             topColors[i] = topColors[i - 4];
             lowColors[i] = topColors[i - 1];
@@ -176,7 +130,6 @@ function shakeColors() {
     }
 }
 
-// put the code for drawing arcs in a separate function
 function drawArcs() {
     for (let i = 0; i < outerCircles.length; i++) {
         for (let j = 0; j < outerCircles[i].length; j++) {
